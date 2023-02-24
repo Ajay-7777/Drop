01fc00818b056d9a7c129acc4fc382a24a4e2b4f
Ajay Panchal <ajaypanchal7778@gmail.com>
2023-02-24 13:31:51 +0530

    f

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 dotenv.config()
 import fileUpload from "express-fileupload"
-import{uploadObject,listObjects,deleteObject} from "./up.js"
+
+import { uploadObject,DownloadObjects } from "./up.js"
 import cookieParser from 'cookie-parser'
 import './models/database.js'
 import files from './models/files.js'
@@ -11,123 +12,131 @@ const app = express();
 
 app.use(fileUpload());
 app.use(express.json());
-app.use(cookieParser()); 
-app.set('view engine','ejs');
+app.use(cookieParser());
+app.set('view engine', 'ejs');
 
-import {OAuth2Client}  from 'google-auth-library';
-let CLIENT_ID="99374051558-r1kkeqccgt3d59fk7qofe23kf8cn58oa.apps.googleusercontent.com"
+import { OAuth2Client } from 'google-auth-library';
+let CLIENT_ID = "99374051558-r1kkeqccgt3d59fk7qofe23kf8cn58oa.apps.googleusercontent.com"
 const client = new OAuth2Client(CLIENT_ID);
 
-app.get('/',(req,res)=>{
-res.render('login')
+app.get('/', (req, res) => {
+    res.status(200)
+    .set('Cache-Control', 'no-cache, no-store, must-revalidate')
+    .set('Pragma', 'no-cache')
+    .set('Expires', '0')
+    .render('login')
 })
-  
-// app.get('/files',async (req,res)=>{
-  
-//    let links = await listObjects();
-   
-//    res.json(links)
-// })
 
 
-app.post('/upload',async (req,res)=>{
+app.post('/dashboard', async (req, res) => {
     let token = req.cookies['session-token'];
     // console.log(token)
-    
-      
-      
+let user={};
     async function verify() {
-        const ticket = await client.verifyIdToken({  
+        const ticket = await client.verifyIdToken({
             idToken: token,
-            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-            // Or, if multiple clients access the backend:
-            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+            audience: CLIENT_ID,
         });
         const payload = ticket.getPayload();
         // const userid = payload['sub'];
-        if(payload.email_verified){
-            let result=await files.find({email:payload.email})
-                    
-                let newfile = new files({
-                    name:payload.name,
-                    email:payload.email,
-                    password: payload.at_hash,
-                    data:req.files.file.name
-                })
-                newfile.save();
-                console.log("newfile",newfile)
+        if (payload.email_verified) {
+            let result = await files.find({ email: payload.email })
+            let d = await files.find({ 
+                email:payload.email,
+                data:req.files.file.name });
+                if(d.length<1){
+            let newfile = new files({
+                name: payload.name,
+                email: payload.email,
+                password: payload.at_hash,
+                data: req.files.file.name
+            })
+            newfile.save();}
+           user.email=payload.email;
         }
-        // If request specified a G Suite domain:
-        // const domain = payload['hd'];
-        // console.log(payload)
-      }
-      verify()
-      .catch(console.error);
-        // If request specified a G Suite domain:
-        // const domain = payload['hd'];
-        // console.log(payload)
-    uploadObject(req.files.file.name,req.files.file.data);
-   res.redirect('/dashboard');
- })
+    }
+    try {
+        await verify();
+        let links=await uploadObject(req.files.file.name, req.files.file.data,user.email);        
+        return res.status(200)
+        .set('Cache-Control', 'no-cache, no-store, must-revalidate')
+        .set('Pragma', 'no-cache')
+        .set('Expires', '0')
+       .render("dashboard",{links});
+    } catch (error) {
+        console.error(error);
+        
+        res.status(500).send('An error occurred');
+    }
+})
 
-app.get('/login',(req,res)=>{
-    res.render('login');
+app.get('/login', (req, res) => {
+    res.status(200)
+    .set('Cache-Control', 'no-cache, no-store, must-revalidate')
+    .set('Pragma', 'no-cache')
+    .set('Expires', '0')
+    .render('login');
 
 })
-app.post('/login',async (req,res)=>{
-  
-    let token=req.body.token;
-    console.log(req.body)
-    
-      
-      
+app.get('/dashboard/:filename',checkAuthenticated, (req, res) => {
+  console.log(req.params.filename)
+  DownloadObjects(req.params.filename,res);
+  return;   
+})
+app.post('/login', async (req, res) => {
+
+    let token = req.body.token;
     async function verify() {
-        const ticket = await client.verifyIdToken({  
+        const ticket = await client.verifyIdToken({
             idToken: token,
-            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-            // Or, if multiple clients access the backend:
-            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+            audience: CLIENT_ID,
         });
         const payload = ticket.getPayload();
         // const userid = payload['sub'];
-        if(payload.email_verified){
-            let result=await files.find({email:payload.email})
-                    if(result.length<1){
-                let newfile = new files({
-                    name:payload.name,
-                    email:payload.email,
+        if (payload.email_verified) {
+            let result = await files.find({ email: payload.email })
+            if (result.length < 1) {
+                let newfile = new files({ 
+                    name: payload.name,
+                    email: payload.email,
                     password: payload.at_hash,
-                    data:""
+                    data: ""
                 })
                 newfile.save();
-                console.log("newfile",newfile)
+                console.log("newfile", newfile)
             }
         }
-        // If request specified a G Suite domain:
-        // const domain = payload['hd'];
-        // console.log(payload)
-      }
-      verify().then(()=>{
 
-        res.cookie('session-token',token);
+    }
+    verify().then(() => {
+
+        res.cookie('session-token', token);
         res.send('success');
-      })
-      .catch(console.error);
-     
-})
-
-app.get('/dashboard',checkAuthenticated,async (req,res)=>{
-    let user=req.user;
-    let links=[];
-    let ress=await files.find({email:user.email})
-        ress.forEach((ele)=>{ 
-            if(ele.data.length>0){
-            links.push("https://aws-bucketnode.s3.amazonaws.com/" + ele.data);}
     })
-    res.render('dashboard',{user,links});
+        .catch(console.error);
+
 })
 
-function checkAuthenticated(req, res, next){
+app.get('/dashboard', checkAuthenticated, async (req, res) => {
+    let user = req.user;
+    let links = [];
+    let ress = await files.find({ email: user.email });
+    ress.forEach ((ele) => {
+
+        if (ele.data.length > 0) {
+          
+            links.push(ele.data);
+        }
+    })
+    res.status(200)
+    .set('Cache-Control', 'no-cache, no-store, must-revalidate')
+    .set('Pragma', 'no-cache')
+    .set('Expires', '0')
+    .render('dashboard', { user,links });
+})
+
+
+function checkAuthenticated(req, res, next) {
 
     let token = req.cookies['session-token'];
 
@@ -141,69 +150,48 @@ function checkAuthenticated(req, res, next){
         user.name = payload.name;
         user.email = payload.email;
         user.picture = payload.picture;
-        user.email_verified=payload.email;
-         
-      }
-     
-verify().then(()=>{
+        user.email_verified = payload.email;
 
-          req.user = user;
-          next();
-      })
-      .catch(err=>{
-          res.redirect('/login')
-      })
+    }
+
+    verify().then(() => {
+
+        req.user = user;
+        next();
+    })
+        .catch(err => {
+            res.render('login')
+        })
 
 }
 
-// app.get('/protectedroute',checkAuthenticated,async (req,res)=>{
-
-// let token = req.cookies['session-token'];
-
-// async function verify() {
-//     const ticket = await client.verifyIdToken({
-//         idToken: token,
-//         audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-//     });
-//     const payload = ticket.getPayload();
-//     return payload;
-//   }
-//  let x= await verify();
- 
-// if(x.email_verified){
-// let result=await files.find({email:x.email})
-//         if(result.length<1){
-//     files.insertMany({
-//         name:x.name,
-//         email:x.email,
-//         password: x.at_hash,
-//     })
-//     let r = await files.find({
-//         email:x.email
-//     })
-//     let tkn=jwt.sign({id:r._id},secret);
-//         res.send({
-//             auth:true,
-//             token:tkn
-//         })
-  
-//         }else{
-//             let tokn=jwt.sign({id:result._id},secret);
-//             res.send({
-//                 auth:true,
-//                 token:tokn
-//             })
-//         }
-
-   
+// app.get('/logout', (req, res) => { 
+//     try{
+//     res.clearCookie('session-token')  
+//     res.status(200).render('login');
+//     statuscode=200;
 // }
+// catch{
+//     res.status(500);
+//     }
 // })
-app.get('/logout',(req,res)=>{
-    res.clearCookie('session-token');
-    res.redirect('/login');
-})
-app.listen(3000,()=>{ 
+app.get('/logout', (req, res) => { 
+    try {
+      res.clearCookie('session-token');  
+      res.status(200)
+         .set('Cache-Control', 'no-cache, no-store, must-revalidate')
+         .set('Pragma', 'no-cache')
+         .set('Expires', '0')
+         .render('login');
+    } catch (error) {
+      res.status(500).send("Internal server error");
+    }
+  });
+  
+  
+app.listen(3000, () => {
     console.log("app running");
 });
+ 
 
 
--- up.js
+++ up.js
@@ -1,21 +1,29 @@
 // Import required AWS SDK clients and commands for Node.js.
 import * as dotenv from 'dotenv'
 dotenv.config()
+import files from './models/files.js'
 import './models/database.js'
-import { PutObjectCommand, CreateBucketCommand,ListObjectsCommand ,S3Client,DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { PutObjectCommand,S3Client,GetObjectCommand} from "@aws-sdk/client-s3";
+import aws from 'aws-sdk'
 
 const credentials={
 secretAccessKey:process.env.ACCESS_SECRET,
 accessKeyId:process.env.ACCESS_KEY
 }
+aws.config.update({                 
+  secretAccessKey: process.env.ACCESS_SECRET,
+  accessKeyId: process.env.ACCESS_KEY,
+  region: process.env.REGION,
+
+});
 const s3Client=new S3Client({
     region:process.env.REGION,
     credentials:credentials
 })
 // Set the parameters
 
-  
-export const uploadObject=async (name,data,token)=>{
+const s3=new aws.S3();
+export const uploadObject=async (name,data,email)=>{
   const params = {
     Bucket: process.env.BUCKET , // The name of the bucket. For example, 'sample-bucket-101'.
     Key: name, // The name of the object. For example, 'sample_upload.txt'.
@@ -24,70 +32,92 @@ export const uploadObject=async (name,data,token)=>{
     
   };
   
-  
-        const results =  s3Client.send(new PutObjectCommand(params));
-        console.log(
+const results =await   s3Client.send(new PutObjectCommand(params));
+         console.log(
             "Successfully uploaded " +
             params.Key +
             " and uploaded it to " +   
             params.Bucket +
-            "/" +
+            "/" + 
             params.Key
         );
-        return results; // For unit tests.
+        let link = [];
+        let ress = await files.find({ email:email });
+        ress.forEach (async (ele) => {
+            if (ele.data.length > 0) { 
+                link.push(ele.data);}
+        })
+        return link; // For unit tests.
 }
 
 
-export const deleteObject = async (params) =>{
-  // Create an Amazon S3 bucket.
+// export const deleteObject = async (params) =>{
+//   // Create an Amazon S3 bucket.
+// //   try {
+// //     const data = await s3Client.send(
+// //         new CreateBucketCommand({ Bucket: params.Bucket })
+// //     );
+// //     console.log(data);
+// //     console.log("Successfully created a bucket called ", data.Location);
+// //     return data; // For unit tests.
+// //   } catch (err) {
+// //     console.log("Error", err);
+// //   }
+//   // Create an object and upload it to the Amazon S3 bucket.
 //   try {
-//     const data = await s3Client.send(
-//         new CreateBucketCommand({ Bucket: params.Bucket })
+//     const results = await s3Client.send(new DeleteObjectCommand(params));
+//     console.log(
+//         "Successfully deleted " +
+//         params.Key +
+//         " and deleted it from " +
+//         params.Bucket +
+//         "/" +
+//         params.Key
 //     );
-//     console.log(data);
-//     console.log("Successfully created a bucket called ", data.Location);
-//     return data; // For unit tests.
+//     return results; // For unit tests.
 //   } catch (err) {
 //     console.log("Error", err);
 //   }
-  // Create an object and upload it to the Amazon S3 bucket.
+// };
+
+// export const listObjects = async () => {
+//   const params={
+//     Bucket:process.env.BUCKET,
+    
+//     ACL:"public-read"
+//   }
+//     // Create an object and upload it to the Amazon S3 bucket.
+//     try {
+//       const results = await s3Client.send(new ListObjectsCommand(params));
+  
+//       let links=[];
+//       if(results.Contents!==null){
+//      for(let item of results.Contents){
+//          links.push("https://aws-bucketnode.s3.amazonaws.com/" + item.Key);
+//         //  console.log("https://aws-bucketnode.s3.amazonaws.com/" + item.Key)
+//      }
+//     }
+//      return links;
+    
+//     } catch (err) {
+//       console.log("Error", err);
+//     }
+//   };
+export const DownloadObjects = async (name, res) => {
+  const params = {
+    Bucket: process.env.BUCKET,
+    Key: name,
+  };
+
   try {
-    const results = await s3Client.send(new DeleteObjectCommand(params));
-    console.log(
-        "Successfully deleted " +
-        params.Key +
-        " and deleted it from " +
-        params.Bucket +
-        "/" +
-        params.Key
-    );
-    return results; // For unit tests.
+    let x = await s3.getObject(params).promise();
+    console.log("downloaded")
+    res.send(x.Body);
+    // const str=CircularJSON.stringify(results.Body)
+  
   } catch (err) {
     console.log("Error", err);
   }
 };
 
-export const listObjects = async () => {
-  const params={
-    Bucket:process.env.BUCKET,
-    
-    ACL:"public-read"
-  }
-    // Create an object and upload it to the Amazon S3 bucket.
-    try {
-      const results = await s3Client.send(new ListObjectsCommand(params));
-  
-      let links=[];
-      if(results.Contents!==null){
-     for(let item of results.Contents){
-         links.push("https://aws-bucketnode.s3.amazonaws.com/" + item.Key);
-        //  console.log("https://aws-bucketnode.s3.amazonaws.com/" + item.Key)
-     }
-    }
-     return links;
-    
-    } catch (err) {
-      console.log("Error", err);
-    }
-  };
-  
+ 
